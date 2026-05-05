fb458895ced7b838e64b72ca0f462a2407e0bd01
jjahamez <stephenchenstephenchen1211@gmail.com>
2026-05-05 06:00:37 -0400

    Small edit to televison view

--- src/views/television/TelevisionView.tsx
+++ src/views/television/TelevisionView.tsx
@@ -4,28 +4,23 @@ import type { TvResponse } from '@/core/types';
 import { useTmdb } from '@/hooks';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
- 
+
 export const TelevisionView = () => {
   const navigate = useNavigate();
-  const [page, setPage] = useState<number>(1);
+  const [page, setPage] = useState(1);
   const { category } = useParams();
-  const safeCategory = category ?? 'airing_today';
-  const { data } = useTmdb<TvResponse>(`${TV_ENDPOINT}/${safeCategory}`, { page }, [safeCategory, page]);
- 
-  const gridData = (data?.results ?? []).map((result) => ({
-    id: result.id,
-    imagePath: result.poster_path,
-    primaryText: result.name,
-  }));
- 
+
+  const { data } = useTmdb<TvResponse>(`${TV_ENDPOINT}/${category}`, { page }, [category, page]);
+  const gridData = (data?.results ?? []).map((result) => ({ id: result.id, imagePath: result.poster_path, primaryText: result.name, }));
+
   useEffect(() => {
     setPage(1);
-  }, [safeCategory]);
- 
+  }, [category]);
+
   if (!data) {
     return <p className="text-center text-gray-400">Loading...</p>;
   }
- 
+
   return (
     <section className="max-w-[1200px] mx-auto p-5 space-y-5">
       <LinkGroup
